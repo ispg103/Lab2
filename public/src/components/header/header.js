e9b339c8fef449d5a9bc942b82008f8726f9ca20
Your Name <you@example.com>
2023-03-09 16:09:42 -0500

    commit

--- public/src/components/header/header.js
+++ public/src/components/header/header.js
@@ -22,8 +22,8 @@ class Header extends HTMLElement {
         this.shadowRoot.innerHTML = `
         <link rel="stylesheet" href="./src/components/header/header.css">
         <div class="barra">
-        <img src="icons/outline_menu_white_48dp.png" class="image">
-        <div class="shows">TV Shows </div>
+        <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" class="image">
+        <div class="home">Home </div>
         <div class="movies"> Movies </div>
         <div class="kids"> Kids </div>
         <img src="icons/outline_person_outline_white_48dp.png" class="image" id="person">
